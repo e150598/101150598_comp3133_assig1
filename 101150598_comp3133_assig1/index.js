0e11e219938a93d373fa1d991820dd9420bd3f89
Elvin Hatamov <101150598@georgebrown.ca>
2021-02-21 18:58:06 -0500

    first commit

---
+++ 101150598_comp3133_assig1/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const TypeDefs = require('./schema')
+const Resolvers = require('./resolvers')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { ApolloServer } = require('apollo-server-express')
+
+//Store sensetive information to env variables
+const dotenv = require('dotenv')
+dotenv.config()
+
+//mongoDB Atlas Connection String
+const url = process.env.MONGODB_URL
+
+//Connect to mongoDB Atlas
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+connect.then(
+  (db) => {
+    console.log(' Connection has done !')
+  },
+  (err) => {
+    console.log(err)
+  }
+)
+
+//Define Apollo Serverempl
+const server = new ApolloServer({
+  typeDefs: TypeDefs.typeDefs,
+  resolvers: Resolvers.resolvers,
+})
+
+//Define Express Server
+const app = express()
+app.use(bodyParser.json())
+app.use('*', cors())
+server.applyMiddleware({ app })
+app.listen({ port: process.env.PORT }, () =>
+  console.log(
+    `Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`
+  )
+)
